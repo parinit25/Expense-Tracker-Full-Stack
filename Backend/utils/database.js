31ef80a7cf406038870f2a52511b1e2e908ecdef
parinit25 <parinit.singh06@gmail.com>
2024-07-15 18:36:11 +0530

    env variables added

--- Backend/utils/database.js
+++ Backend/utils/database.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  database: "expense-tracker",
-  host: "localhost",
-  username: "root",
-  password: "Node@12345",
-  dialect: "mysql",
-  timezone: "+05:30"
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dialect: process.env.DB_DIALECT,
+  timezone: process.env.DB_TIMEZONE,
 });
 
 module.exports = sequelize;
